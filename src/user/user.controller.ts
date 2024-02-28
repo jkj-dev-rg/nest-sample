64be3454ba3a332e9e71c99ed838877049738a58
jkj-rg <jyothin@revyrieglobal.com>
2024-02-28 12:41:34 +0530

    added basic CRUD APIs with dummy returns

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -8,5 +16,25 @@ export class UserController {
   @Post()
   create(@Body() bodyData) {
     return this.userService.create();
+  }
+
+  @Get()
+  getAll() {
+    return this.userService.getAll();
+  }
+
+  @Get(':userId')
+  getById(@Param() param) {
+    return this.userService.getById();
+  }
+
+  @Patch(':userId')
+  update() {
+    return this.userService.update();
+  }
+
+  @Delete(':userId')
+  remove() {
+    return this.userService.remove();
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,4 +5,16 @@ export class UserService {
   create() {
     return 'user created';
   }
+  getAll() {
+    return 'all user retrived';
+  }
+  getById() {
+    return 'get user by Id';
+  }
+  update() {
+    return 'update user by id';
+  }
+  remove() {
+    return 'user removed';
+  }
 }
