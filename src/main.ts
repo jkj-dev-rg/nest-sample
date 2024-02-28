119dc22ba944e7c1eca02a7f0a09937f395bfccc
jkj-rg <jyothin@revyrieglobal.com>
2024-02-28 14:51:21 +0530

    db connection established

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppDataSource } from './db/db-connection';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  AppDataSource.initialize()
+    .then(() => {
+      console.log('db connection ok');
+      app
+        .listen(3000)
+        .then(() => console.log('server connected to port 3000'))
+        .catch((err) => console.log('error occured in server connection'));
+    })
+    .catch((error) => console.log(error));
 }
 bootstrap();
